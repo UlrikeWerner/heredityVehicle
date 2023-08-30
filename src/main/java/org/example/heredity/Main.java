e6e8c5a97591c83c845b0b5505ad5e6e7cb407e1
UlrikeWerner <ulrike_werner@web.de>
2023-08-30 11:22:15 +0200

    add class Motorcycle and print information in Main

--- src/main/java/org/example/heredity/Main.java
+++ src/main/java/org/example/heredity/Main.java
@@ -6,8 +6,12 @@ public class Main {
         v1.setConstructionYear(2022);
         Car c1 = new Car("Opel", "CorsaC", 4);
         c1.setConstructionYear(2012);
+        Motorcycle m1 = new Motorcycle("Honda", "Chopper", "red");
+        m1.setConstructionYear(2001);
         System.out.println(v1);
         System.out.println();
         System.out.println(c1);
+        System.out.println();
+        System.out.println(m1);
     }
 }
